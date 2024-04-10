ebbb95eee05663962ac54edb99a88834cf6b47fc
Norbiox <norbertchmiel.it@gmail.com>
2024-04-10 09:54:20 +0200

    add victor-mono font

--- .post_install/30_look_and_feel.sh
+++ .post_install/30_look_and_feel.sh
@@ -1,5 +1,5 @@
 # Install lxappearence
-sudo dnf install lxappearence
+sudo dnf install lxappearance
 
 # Install themes
 sudo dnf install arc-theme
@@ -9,3 +9,6 @@ gsettings set org.gnome.desktop.interface gtk-theme Arc-Dark-solid
 
 # Install fonts
 sudo dnf install fontawesome-fonts
+wget https://rubjo.github.io/victor-mono/VictorMonoAll.zip
+unzip VictorMonoAll.zip -d ~/.local/share/fonts/
+fc-cache -f
